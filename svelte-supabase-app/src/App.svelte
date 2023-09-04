00e904c2bee264097ff58d4b52d02cb50f0e24a7
xYooki <yukitheegg@gmail.com>
2023-09-05 09:36:10 +1000

    Changed Icon and removed the filler information

--- svelte-supabase-app/src/App.svelte
+++ svelte-supabase-app/src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Kangan Quiz</h1>
+	<p>INSERT STUFF HERE</p>
 </main>
 
 <style>
