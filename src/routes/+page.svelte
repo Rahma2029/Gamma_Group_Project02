44aefb5bfbe99c02bb3cfa1479ac081d2638e933
xYooki <yukitheegg@gmail.com>
2023-09-08 12:59:10 +1000

    Added the github signin code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,57 @@
 <script>
-	 
-	// Function to determine which login logic to execute based on user type
-	async function handleLogin() {
-		const userType = 'Admin'; // Replace with your logic to determine the user type
+	import { createClient } from '@supabase/supabase-js'
+	import { onMount } from 'svelte';
 
-		if (userType === 'Admin') {
-		await handleAdminLogin();
-		} 
-		else if (userType === 'User') {
-		await handleUserLogin();
-		} 
-		else {
-		console.error('Invalid user type.');
-		}
-	}
+	const supabaseUrl = 'https://spcbocsicbrcuctlwwqc.supabase.co'
+	const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwY2JvY3NpY2JyY3VjdGx3d3FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMyODQ3NTEsImV4cCI6MjAwODg2MDc1MX0.jK2FAWoHlw6YkDRxuNKWfEeAZYh_OGOjSDkWJqOW2J4'
+	const supabase = createClient(supabaseUrl, supabaseKey)
 
-		import GitHubSignIn from "./GitHubSignIn.svelte";
+//LOGIN INPUTS
+
+let username = '';
+let password = '';
+
+//ON MOUNT SYNC
+	onMount(async () => {
+    user = await supabase.auth.getUser();
+    console.log("onMount -> user:", user);
+
+    if (user) {
+        console.log("onMount -> user:", user);
+    } else {
+        user = null;
+    }
+});
+
+	// GITHUB SIGN IN FUNCTION
+	async function signInGitHub() {
+    console.log("signInGitHub");
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'github',
+    });
+    if(data) {
+        console.log("data:", data);
+        user = await supabase.auth.getUser();
+        console.log("user:", user);
+    }
+    if(error) {
+        console.log("error:", error);
+    }
+}
+async function signout() {
+    const { error } = await supabase.auth.signOut();
+    if(error) {
+        console.log("error:", error);
+    }
+    user = null;
+}
+ 
 		import Navbar from './navbar.svelte';
 	// Call the handleLogin function when a login action occurs (e.g., button click)
 	/* async function handleLoginButtonClick() {
 		await handleLogin();
 	}*/
 	</script> 
-
-	
-
 
 <main>
 		<Navbar />
@@ -33,7 +60,10 @@
 	<!--  <Login /> -->
 		<div>
 		<!-- Other content -->
-		<GitHubSignIn />
+		<input type="text" placeholder="Username" bind:value="{username}" />
+		<input type="password" placeholder="Password" bind:value="{password}" />
+		<button on:click="{handleLogin}">Login</button>
+		<button on:click={()=>signInGitHub()}>GitHub SignIn</button>
 		</div>
 	</main>
 
--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,75 +1,62 @@
-<!-- src/routes/Login.svelte -->
 <script>
-import Login from './Login.svelte';
-    // import Input  from './Input.svelte'
-	/* export let name; */
-		// [ADMIN LOGIN] Function for handling admin login
-		
-async function handleAdminLogin() {
-  try {
-    const { data, error } = await supabase
-      .from('users') // Replace 'user_table' with your actual table name
-      .select('*')
-      .eq('username', providedUsername); // Replace 'username' with your column name
+	import { createClient } from '@supabase/supabase-js'
+  import { onMount } from 'svelte';
+	const supabaseUrl = 'https://spcbocsicbrcuctlwwqc.supabase.co'
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwY2JvY3NpY2JyY3VjdGx3d3FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMyODQ3NTEsImV4cCI6MjAwODg2MDc1MX0.jK2FAWoHlw6YkDRxuNKWfEeAZYh_OGOjSDkWJqOW2J4'
+	const supabase = createClient(supabaseUrl, supabaseKey)
 
-  if (error) {
-    console.error('Database error:', error.message);
-    return; // Return early if there's an error
-  }
+	// Function to determine which login logic to execute based on user type
+	async function handleLogin() {
+		const userType = 'Admin'; // Replace with your logic to determine the user type
 
-  if (data && data.length > 0) {
-    const user = data[0];
-    const storedPassword = user.password; // Retrieve the hashed and salted password
-    const passwordMatches = await comparePasswords(providedPassword, storedPassword);
+		if (userType === 'Admin') {
+		await handleAdminLogin();
+		} 
+		else if (userType === 'User') {
+		await handleUserLogin();
+		} 
+		else {
+		console.error('Invalid user type.');
+		}
+	}
 
-      if (passwordMatches && user.role === 'Admin') {
-        // Password matches, user is an admin
-        // Proceed with admin authentication logic
-      } else {
-        console.error('Admin credentials are not valid.');
-      }
-    } else {
-      console.error('Admin not found.');
-    }
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-}
+		import GitHubSignIn from "./GitHubSignIn.svelte";
+		import Navbar from './navbar.svelte';
+	// Call the handleLogin function when a login action occurs (e.g., button click)
+	/* async function handleLoginButtonClick() {
+		await handleLogin();
+	}*/
+	</script> 
 
-// [USER LOGIN] Function for handling user login
-async function handleUserLogin() {
-  try {
-    const {data, error} = await supabase
-      .from('users') // Replace 'user_table' with your actual table name
-      .select('*')
-      .eq('username', providedUsername); // Replace 'username' with your column name
+<main>
+		<Navbar />
 
-    if (error) {
-      console.error('Database error:', error.message);
-      return; // Return early if there's an error
-    }
+		<h1>Kangan Quiz</h1>
+	<!--  <Login /> -->
+		<div>
+		<!-- Other content -->
+		<GitHubSignIn />
+		</div>
+	</main>
 
-    if (data && data.length > 0) {
-      const user = data[0];
-      const storedPassword = user.password; // Retrieve the hashed and salted password
-      const passwordMatches = await comparePasswords(providedPassword, storedPassword);
-      if (passwordMatches && user.role === 'User') {
-        // Password matches, user is authenticated
-        const userRole = user.role; // Retrieve the user's role
-        // Proceed with user authentication logic based on the role
-      }
-      else {
-          console.error('Password does not match or user is not a User.');
-      }
-    } 
-    else {
-      console.error('User not found.');
-    }
-  }
-  catch (error) {
-    console.error('Error:', error.message);
-  }
-}
+	<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
 
-</script>
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
 
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}  </style>
+  
